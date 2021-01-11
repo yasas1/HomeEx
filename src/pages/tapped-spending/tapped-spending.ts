6173c386f5c22fe3c9cbe9e549b223316034be43
yasas1 <32437531+yasas1@users.noreply.github.com>
2021-01-11 16:02:37 +0530

    home implementation

--- src/pages/tapped-spending/tapped-spending.ts
+++ src/pages/tapped-spending/tapped-spending.ts
@@ -18,17 +18,36 @@ export class TappedSpendingPage {
 
   selectedSpending: Spending;
 
+  public today: any;
+  public spendingDate:any;
+
+  private newDateToday = new Date();
+
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     private datePipe: DatePipe,
     ) {
 
+    this.today = this.transformDateFormat2( this.newDateToday);
     this.selectedSpending = navParams.get('spending');
+    this.spendingDate = this.transformDateFormat3( new Date(this.selectedSpending.date));
   }
 
   ionViewDidLoad() {
     
+  }
+
+  private transformDateFormat1(date):string {
+    return this.datePipe.transform(date, 'yyyy-MM-dd'); 
+  }
+
+  private transformDateFormat2(date):string {
+    return this.datePipe.transform(date, 'MMMM dd yyyy'); 
+  }
+
+  private transformDateFormat3(date):string {
+    return this.datePipe.transform(date, 'EEEE MMMM dd yyyy'); 
   }
 
 }
